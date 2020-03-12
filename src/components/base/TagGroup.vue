9697ba7ba0e7d0b17ac0d529d1a22caaa75da910
lanmeizi <957801791@qq.com>
2020-03-12 23:36:18 +0800

    搜索组件

---
+++ src/components/base/TagGroup.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="tag-group-wrapper">
+    <div class="tag-group-header">
+      <div class="header-text">{{ headerText }}</div>
+      <div class="header-btn" @click="onBtnClick">{{ btnText }}</div>
+    </div>
+    <div class="tag-group">
+      <div class="tag-group-inner" v-for="(text, index) in value" :key="index">
+        <Tag :text="text" @onClick="onTagClick(text, index)" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Tag from './Tag'
+export default {
+  props: {
+    headerText: String,
+    btnText: String,
+    value: Array
+  },
+  data() {
+    return {}
+  },
+  components: {
+    Tag
+  },
+  methods: {
+    onTagClick(text, index) {
+      this.$emit('onTagClick', text, index)
+    },
+    onBtnClick() {
+      this.$emit('onBtnClick')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.tag-group-wrapper {
+  .tag-group-header {
+    display: flex;
+    justify-content: space-between;
+    padding: 0 16.5px;
+    font-size: 14px;
+    line-height: 20px;
+    .header-text {
+      color: #333;
+    }
+    .header-btn {
+      color: #3698EF;
+    }
+  }
+  .tag-group {
+    display: flex;
+    flex-flow: row wrap;
+    padding: 4px 10px 0 10px;
+    .tag-group-inner {
+      padding: 12px 6px 0 6px;
+    }
+  }
+}
+</style>
--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -1,20 +1,78 @@
 <template>
   <div>
-    <Tag text="测试文本" @onClick="onTagClick" />
+    <!-- <TagGroup
+      :value="tags"
+      header-text="热门搜索"
+      btn-text="换一批"
+      @onTagClick="onTagClick"
+      @onBtnClick="onBtnClick"
+    />
+    <SearchItem
+      icon="apps-o"
+      title="计算机科学"
+      sub-title="类别"
+    /> -->
+    <SearchTable :data="list" />
   </div>
 </template>
 
 <script>
-import Tag from '../../components/base/Tag'
+import TagGroup from '../../components/base/TagGroup'
+import SearchItem from '../../components/search/SearchItem'
+import SearchTable from '../../components/search/SearchTable'
 export default {
   data() {
-    return {}
+    return {
+      list: [
+        {
+          id: 1,
+          cover: 'http://img3m1.ddimg.cn/45/32/1011676491-1_u_6.jpg',
+          title: '树形结构树形结构树形结构树形结构树形结构树形结构树形结构树形结构树形结构树形结构树形结构树形结构树形结构',
+          author: '兰兰妹子',
+          categoryText: '测试数据'
+        },
+        {
+          id: 5,
+          cover: 'http://img3m1.ddimg.cn/45/32/1011676491-1_u_6.jpg',
+          title: '树形结构',
+          author: '兰兰妹子',
+          categoryText: '测试数据'
+        },
+        {
+          id: 2,
+          cover: 'http://img3m1.ddimg.cn/45/32/1011676491-1_u_6.jpg',
+          title: '树形结构',
+          author: '兰兰妹子',
+          categoryText: '测试数据'
+        },
+        {
+          id: 3,
+          cover: 'http://img3m1.ddimg.cn/45/32/1011676491-1_u_6.jpg',
+          title: '树形结构',
+          author: '兰兰妹子',
+          categoryText: '测试数据'
+        },
+        {
+          id: 4,
+          cover: 'http://img3m1.ddimg.cn/45/32/1011676491-1_u_6.jpg',
+          title: '树形结构',
+          author: '兰兰妹子',
+          categoryText: '测试数据'
+        }
+      ]
+    }
   },
   components: {
-    Tag
+    TagGroup,
+    SearchItem,
+    SearchTable
   },
   methods: {
-    onTagClick() {
+    onTagClick(text, index) {
+      console.log('tag...', text, index)
+    },
+    onBtnClick() {
+      console.log('btn...')
     }
   }
 }
